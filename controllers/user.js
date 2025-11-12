ab1d890ed4bc9a3f00872b7f0a4919adb5e0a5bd
abhibs <abhirambs97@gmail.com>
2025-11-12 15:21:17 +0530

    user register and login api with only send message

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,11 @@
 export const aboutMe = (req, res) => {
   res.send('Abhiram B S Javalli Tudoor Thirthahalli')
 }
+
+export const userlogin = (req, res) => {
+  res.send('User Login')
+}
+
+export const userRegister = (req, res) => {
+  res.send('User Register')
+}
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { aboutMe } from '../controllers/user.js'
+import { aboutMe, userlogin, userRegister } from '../controllers/user.js'
 
 const router = express.Router()
 
 router.get('/abhiram', aboutMe)
+router.post('/login', userlogin)
+router.post('/register', userRegister)
 
 export default router
