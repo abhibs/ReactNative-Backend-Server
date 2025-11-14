1e1fb8f59e3faf7a85ef43583466e8b41ea62f13
abhibs <abhirambs97@gmail.com>
2025-11-14 13:35:34 +0530

    multer middleware file created and use in user route

--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,13 @@ import {
   userRegister,
 } from '../controllers/user.js'
 import { isAuthenticated } from '../middlewares/auth.js'
+import { singleUpload } from '../middlewares/multer.js'
 
 const router = express.Router()
 
 router.get('/abhiram', aboutMe)
 router.post('/login', userlogin)
-router.post('/register', userRegister)
+router.post('/register', singleUpload, userRegister)
 router.get('/profile', isAuthenticated, getProfile)
 router.get('/logout', isAuthenticated, userLogout)
 
