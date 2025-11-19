900a46e49e097ad8efcd77f0f50ce01e27c109e2
abhibs <abhirambs97@gmail.com>
2025-11-19 18:42:30 +0530

    product controller created and use with product routes with simple message

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import { getAllProducts } from '../controllers/product.js'
 
 const router = express.Router()
 
-router.get('/all', (req, res) => {
-  res.send('all product list')
-})
+router.get('/all', getAllProducts)
 
 export default router
