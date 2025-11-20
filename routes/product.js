b8a9c413264d12aa1cbe6dc222bc76161d306605
abhibs <abhirambs97@gmail.com>
2025-11-20 16:13:08 +0530

    project inserted controller and route created

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { addProduct, getAllProducts } from '../controllers/product.js'
 import { isAuthenticated } from '../middlewares/auth.js'
+import { singleUpload } from '../middlewares/multer.js'
 
 const router = express.Router()
 
 router.get('/all', getAllProducts)
-router.post('/add', isAuthenticated, addProduct)
+router.post('/add', isAuthenticated, singleUpload, addProduct)
 
 export default router
