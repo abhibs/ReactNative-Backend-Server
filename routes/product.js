1942f81acd9a6dfe216bc7b649ed5e637ba63632
abhibs <abhirambs97@gmail.com>
2025-12-01 14:12:45 +0530

    admin  middleware created

--- routes/product.js
+++ routes/product.js
@@ -11,26 +11,26 @@ import {
   productDetail,
   updateProduct,
 } from '../controllers/product.js'
-import { isAuthenticated } from '../middlewares/auth.js'
+import { isAdmin, isAuthenticated } from '../middlewares/auth.js'
 import { singleUpload } from '../middlewares/multer.js'
 
 const router = express.Router()
 
 router.get('/all', getAllProducts)
-router.post('/add', isAuthenticated, singleUpload, addProduct)
+router.post('/add', isAuthenticated, isAdmin, singleUpload, addProduct)
 router
   .route('/single/:id')
-  .get(isAuthenticated, productDetail)
-  .put(isAuthenticated, updateProduct)
-  .delete(isAuthenticated, deleteProduct)
+  .get(isAuthenticated, isAdmin, productDetail)
+  .put(isAuthenticated, isAdmin, updateProduct)
+  .delete(isAuthenticated, isAdmin, deleteProduct)
 
 router
   .route('/images/:id')
-  .post(isAuthenticated, singleUpload, addProductImage)
-  .delete(isAuthenticated, deleteProductImage)
+  .post(isAuthenticated, isAdmin, singleUpload, addProductImage)
+  .delete(isAuthenticated, isAdmin, deleteProductImage)
 
-router.post('/category/add', isAuthenticated, addCategory)
+router.post('/category/add', isAuthenticated, isAdmin, addCategory)
 router.get('/category/all', isAuthenticated, getAllCategories)
-router.delete('/category/:id', isAuthenticated, deleteCategory)
+router.delete('/category/:id', isAuthenticated, isAdmin, deleteCategory)
 
 export default router
